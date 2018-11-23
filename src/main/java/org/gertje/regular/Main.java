cbaa405eb28420b931b7597f37584cd15af3e06e
Geert Mulders <g.c.w.m.mulders@gmail.com>
2018-11-23 22:06:30 +0100

    Create initial definition for the lexer-definition-file

--- src/main/java/org/gertje/regular/Main.java
+++ src/main/java/org/gertje/regular/Main.java
@@ -7,6 +7,7 @@ import org.gertje.regular.codegenerator.SourceFile;
 import org.gertje.regular.definition.LexerDefinition;
 import org.gertje.regular.definition.builder.LexerDefinitionBuilder;
 import org.gertje.regular.parser.RegExException;
+import org.gertje.regular.parser.description.ParserException;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -20,29 +21,28 @@ import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, CodeGenerationException {
+	public static void main(String[] args) throws CodeGenerationException, ParserException {
 		new Main().run();
 	}
 
-	public void run() throws IOException, CodeGenerationException {
+	public void run() throws CodeGenerationException, ParserException {
 
-		final LexerDefinition lexerDefinition;
-		try {
-			lexerDefinition = createLexerDef();
-		} catch (RegExException e) {
-			throw new CodeGenerationException("Could not build lexer definition:", e);
-		}
+		final LexerDefinition lexerDefinition = createLexerDef();
 
 		CodeGenerator generator = createCodeGenerator();
 
 		Map<String, Object> generatorConfig = new HashMap<>();
-		generatorConfig.put("packageName", "org.gertje.regular.lexer");
-		generatorConfig.put("lexerName", "LexerDefinition");
+		generatorConfig.put("packageName", "org.gertje.regular.parser.description.lexer");
+		generatorConfig.put("lexerName", "BaseLexerDescription");
 
 		Collection<SourceFile> sourceFiles = generator.generate(lexerDefinition, generatorConfig);
 
 		for (SourceFile sourceFile : sourceFiles) {
-			save(sourceFile);
+			try {
+				save(sourceFile);
+			} catch (IOException e) {
+				throw new CodeGenerationException("The file could not be saved: " + sourceFile.getName(), e);
+			}
 		}
 	}
 
@@ -51,92 +51,33 @@ public class Main {
 	}
 
 	private void save(SourceFile sourceFile) throws IOException {
-//		System.out.println(sourceFile.getName());
-//		System.out.println();
-//		System.out.println();
-//		System.out.print(sourceFile.getContents());
-
 		Path path = Paths.get("target/generated-sources/main/" + sourceFile.getName());
 		Files.createDirectories(path.getParent());
-
-
-		Files.write(path,
-				sourceFile.getContents().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
+		Files.write(path, sourceFile.getContents().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
 	}
 
-	private LexerDefinition createLexerDef() throws RegExException {
-		return new LexerDefinitionBuilder()
-				.lexerStartStateName("DEFAULT")
-				.startLexerClass("DEFAULT")
-					.addLexerToken("WHITE_SPACE", "( |\\t)+", "DEFAULT")
-					.addLexerToken("LEXER_CLASS", "([a-zA-Z_])[a-zA-Z0-9_]*", "DEFAULT")
-					.addLexerToken("TOKEN_NAME", "<([a-zA-Z_])[a-zA-Z0-9_]*>", "DEFAULT")
-					.addLexerToken("REGEX_START", "'", "REGEX")
-					.addLexerToken("NEXT_STATE", "->", "DEFAULT")
-				.end()
-				.startLexerClass("REGEX")
-					.addLexerToken("REGEX_END", "'", "DEFAULT")
-					.addLexerToken("REGEX_PART", "[^'\\\\]*", "REGEX")
-					.addLexerToken("REGEX_SINGLE_QUOTE", "\\\\'", "REGEX")
-					.addLexerToken("REGEX_TAB", "\\\\t", "REGEX")
-					.addLexerToken("REGEX_NEW_LINE", "\\\\n", "REGEX")
-					.addLexerToken("REGEX_CARRIAGE_RETURN", "\\\\r", "REGEX")
-					.addLexerToken("REGEX_BACKSLASH", "\\\\\\\\", "REGEX")
-				.end()
-				.build();
+	private LexerDefinition createLexerDef() throws ParserException {
+		try {
+			return new LexerDefinitionBuilder()
+					.lexerStartStateName("DEFAULT")
+					.startLexerClass("DEFAULT")
+						.addLexerToken("NEW_LINE", "\\r\\n|\\r|\\n", "DEFAULT")
+						.addLexerToken("WHITE_SPACE", "( |\\t)+", "DEFAULT")
+						.addLexerToken("LEXER_CLASS", "([a-zA-Z_])[a-zA-Z0-9_]*", "DEFAULT")
+						.addLexerToken("TOKEN_NAME", "<([a-zA-Z_])[a-zA-Z0-9_]*>", "DEFAULT")
+						.addLexerToken("REGEX_START", "'", "REGEX")
+						.addLexerToken("NEXT_STATE", "->", "DEFAULT")
+					.end()
+					.startLexerClass("REGEX")
+						.addLexerToken("REGEX_END", "'", "DEFAULT")
+						.addLexerToken("REGEX_PART", "[^'\\\\]*", "REGEX")
+						.addLexerToken("REGEX_SINGLE_QUOTE", "\\\\\\'", "REGEX")
+						.addLexerToken("REGEX_DOUBLE_BACKSLASH", "\\\\\\\\", "REGEX")
+						.addLexerToken("REGEX_BACKSLASH", "\\\\", "REGEX")
+					.end()
+					.build();
+		} catch (RegExException e) {
+			throw new ParserException("An exception occurred during parsing.", e);
+		}
 	}
-
-	private LexerDefinition createSimpleExpressionLexer() throws RegExException {
-		return new LexerDefinitionBuilder()
-				.lexerStartStateName("DEFAULT")
-				.startLexerClass("DEFAULT")
-					.addLexerToken("WHITE_SPACE", "( |\\t)+", "DEFAULT")
-					.addLexerToken("END_OF_EXPRESSION", ";", "DEFAULT")
-					.addLexerToken("NEW_LINE", "\\r\\n|\\r|\\n", "DEFAULT")
-					.addLexerToken("DATE_START", "D'", "DATE")
-					.addLexerToken("BOOLEAN", "true|false", "DEFAULT")
-					.addLexerToken("IDENTIFIER", "([a-zA-Z_])[a-zA-Z0-9_]*", "DEFAULT")
-					.addLexerToken("LEFT_PARENTHESIS", "\\(", "DEFAULT")
-					.addLexerToken("RIGHT_PARENTHESIS", ")", "DEFAULT")
-					.addLexerToken("LEFT_BRACKET", "{", "DEFAULT")
-					.addLexerToken("RIGHT_BRACKET", "}", "DEFAULT")
-					.addLexerToken("STRING_START", "'", "STRING")
-					.addLexerToken("DECIMAL", "([0-9]+\\.[0-9]*)|([0-9]*\\.[0-9]+)", "DEFAULT")
-					.addLexerToken("INTEGER", "[0-9]+", "DEFAULT")
-					.addLexerToken("BOOLEAN_AND", "&&", "DEFAULT")
-					.addLexerToken("BOOLEAN_OR", "[|][|]", "DEFAULT")
-					.addLexerToken("PLUS", "+", "DEFAULT")
-					.addLexerToken("MINUS", "-", "DEFAULT")
-					.addLexerToken("POWER", "^", "DEFAULT")
-					.addLexerToken("MULTIPLY", "*", "DEFAULT")
-					.addLexerToken("DIVIDE", "/", "DEFAULT")
-					.addLexerToken("PERCENT", "%", "DEFAULT")
-					.addLexerToken("EQ", "==", "DEFAULT")
-					.addLexerToken("NEQ", "!=", "DEFAULT")
-					.addLexerToken("LEQ", "<=", "DEFAULT")
-					.addLexerToken("LT", "<", "DEFAULT")
-					.addLexerToken("GEQ", ">=", "DEFAULT")
-					.addLexerToken("GT", ">", "DEFAULT")
-					.addLexerToken("NOT", "!", "DEFAULT")
-					.addLexerToken("ASSIGNMENT", "=", "DEFAULT")
-					.addLexerToken("IF", "?", "DEFAULT")
-					.addLexerToken("COLON", ":", "DEFAULT")
-					.addLexerToken("COMMA", ",", "DEFAULT")
-				.end()
-				.startLexerClass("STRING")
-					.addLexerToken("STRING_PART", "[^'\\\\]*", "STRING")
-					.addLexerToken("STRING_END", "'", "DEFAULT")
-					.addLexerToken("STRING_SINGLE_QUOTE", "\\\\'", "STRING")
-					.addLexerToken("STRING_TAB", "\\\\t", "STRING")
-					.addLexerToken("STRING_NEW_LINE", "\\\\n", "STRING")
-					.addLexerToken("STRING_CARRIAGE_RETURN", "\\\\r", "STRING")
-					.addLexerToken("STRING_BACKSLASH", "\\\\\\\\", "STRING")
-				.end()
-				.startLexerClass("DATE")
-					.addLexerToken("DATE_PART", "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]", "DATE")
-					.addLexerToken("DATE_END", "'", "DEFAULT")
-				.end()
-				.build();
-	}
-
 }
